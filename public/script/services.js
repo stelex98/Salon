978798293f6f9367ee7b7c403e324832dfce8153
olgagret3 <olushkagret3@gmail.com>
2018-12-01 19:29:15 +0300

    [DEL] delete unnecessary lines

--- public/script/services.js
+++ public/script/services.js
@@ -12,34 +12,11 @@ function getServices() {
         contentType: "application/json",
         dataType: 'json',
         success: function(services) {
-            // let group = groups(services);
-            // for(let i = 0; i < group.length; i++){
-            //  let div = `<div> <h2> ${group[i]} </h2> </div>`;
-            //  for(let j = 0; j < services.length; j++){
-            //    if(services[j].group === group[i])
-            //      div += divs(services[j]);
-            //  }
-            //  $("#service_block").append(div);
-            // }
+            console.log(services);
         }
     });
 }
 
-function groups(services) {
-    let group = [];
-    for (let i = 0; i < services.length; i++) {
-        if (!(group.some((gr) => { return gr === services[i].group }))) {
-            group.push(services[i].group);
-        }
-    }
-    return group;
-}
-
-function divs(service) {
-    return `
-        <a  href='/api/beauty_salon/service/${service.id}'  onclick="return service(this);" ">${service.service}</a><br >
-    `;
-}
 
 function service(element) {
     $.ajax({
