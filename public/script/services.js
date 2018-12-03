434f041478a415b96d5cd376a8c17f6443adcd21
stelex98 <stelex98@mail.ru>
2018-12-03 13:49:14 +0300

    added descirbe/price into service

--- public/script/services.js
+++ public/script/services.js
@@ -2,6 +2,7 @@
 let titleCard = document.getElementsByClassName('.card-image');
 let checkServices = [];
 let checkedServices = [];
+let arrayDataOfService = [];
 
 //для селектов
 //{id: , service: , id_group: , group: }
@@ -11,49 +12,55 @@ function getServices() {
         type: "GET",
         contentType: "application/json",
         dataType: 'json',
-        success: function(services) {
+        success: function (services) {
             console.log(services);
+            service();
         }
     });
 }
 
 //для конкретной услуги
 //
-function service() {
-    //let num_service = 0;
+function service(id, num_service) {
     $.ajax({
-        url: `/api/beauty_salon/services/${num_service}`,
+        url: `/api/beauty_salon/service/${num_service}`,
         type: "GET",
         contentType: "application/json",
         dataType: 'json',
-        success: function(service) {
-            console.log(service);
+        success: function (service) {
+            let price = service[0].price;
+            let describeService = service[0].about_service;
+            let mySelectServices = $("#" + id + " :selected").text();
+
+            $('.card-content')[0].innerText = `${describeService}\nЦена: $${price}`;
+            checkServices.push(id);
+
+            if (checkServices[0] == id) {
+                $("#card-title")[0].innerText = mySelectServices; // запись в карточку названия услуги
+            } else {
+                reset();
+                checkServices[0] = id;
+            }
+            $("#card-title")[0].innerText = mySelectServices; // запись в карточку названия услуги
+
         }
     });
     return false;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 });
 
-function sendNameAndDescribeServices(id, defaultValue) {
-    let mySelectServices = $("#" + id + " :selected").text();
-    checkServices.push(id);
-    if (checkServices[0] == id) {
-        $("#card-title")[0].innerText = mySelectServices; // запись в карточку названия услуги
-    } else {
-        reset();
-        checkServices[0] = id;
-    }
-    $("#card-title")[0].innerText = mySelectServices; // запись в карточку названия услуги
+function sendNameAndDescribeServices(id, defaultValue, value) {
+    service(id, value);
 }
 
 function reset() {
-  var select = $("#" + checkServices[0]);
-  select.prop('selectedIndex', 0); //Sets the first option as selected
-  select.formSelect();        //Update material select
+    var select = $("#" + checkServices[0]);
+    select.prop('selectedIndex', 0); //Sets the first option as selected
+    select.formSelect();        //Update material select
 }
 
 getServices();
